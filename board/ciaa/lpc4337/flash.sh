0cc1e44826553cdfb322b12e675a1080fed462b2
Martin Ribelotta <martinribelotta@gmail.com>
2014-12-08 11:45:02 -0300

    scripts: Add openocd scripts to flash board
    In order to allow the user to program the board's flash with all the
    firmware in a simple fashion, we need a proper openocd configuration
    file which initializes the internal flash and spifi devices
    
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

---
+++ board/ciaa/lpc4337/flash.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+CFG=board/ciaa/lpc4337/ciaa-nxp.cfg
+OOCD=output/host/usr/bin/openocd
+BIMAGE=output/images/u-boot.bin
+KIMAGE=output/images/uImage
+$OOCD -f $CFG \
+	-c "init" \
+	-c "reset halt" \
+	-c "flash write_image erase unlock $KIMAGE 0x14000000 bin" \
+	-c "flash write_image erase unlock $BIMAGE 0x1a000000 bin"
